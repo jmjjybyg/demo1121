0f8bb7d61d0389b46debb511093a28ba85092e71
jmjjybyg <wongshunhing.0431@gmail.com>
2025-11-21 09:22:56 +0800

    Update Businessdashboard.py

--- Businessdashboard.py
+++ Businessdashboard.py
@@ -6,7 +6,7 @@ st.title("Business Dashboard with Streamlit Layouts")
 msg = "Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
 st.write(msg)
 
-col1, col2 = st.column(2)
+col1, col2 = st.columns(2)
 
 with col1:
     st.header("Col 1")
